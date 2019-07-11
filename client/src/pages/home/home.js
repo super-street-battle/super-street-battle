05bc88dc93e06e01c6ec90f3bbe37aa0bf4a3bb8
kaylasf <sanfilippokayla@gmail.com>
2019-07-11 11:06:32 -0700

    garage tab. working on dummy data and display

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -10,68 +10,71 @@ import Challenges from '../../components/challenges'
 import Result from '../../components/result'
 import Topscore from '../../components/topscore'
 import Players from '../../components/players'
+import Garage from '../garage'
 import './home.css'
 
 function TabContainer({ children, dir }) {
-    return (
-      <Typography component="div" dir={dir} style={{ padding: 8 * 3 }}>
-        {children}
-      </Typography>
-    );
-  }
-  
-  TabContainer.propTypes = {
-    children: PropTypes.node.isRequired,
-    dir: PropTypes.string.isRequired,
-  };
-  
-  const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1,
-        width: '100%',
-        backgroundColor: theme.palette.background.paper,
-    },
-  }));
-  
+  return (
+    <Typography component="div" dir={dir} style={{ padding: 8 * 3 }}>
+      {children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+  dir: PropTypes.string.isRequired,
+};
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
 const Home = _ => {
-    const classes = useStyles();
-    const theme = useTheme();
-    const [value, setValue] = React.useState(0);
+  const classes = useStyles();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
 
-    function handleChange(event, newValue) {
-        setValue(newValue);
-    }
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
 
-    function handleChangeIndex(index) {
-        setValue(index);
-    }
-    return (
-        <div className={classes.root} id="homenav">
-            <AppBar position="static" color="blue" className="appbar">
-                <Tabs className="tabs"
-                value={value}
-                onChange={handleChange}
-                variant="scrollable"
-                scrollButtons="auto"
-                >
-                <Tab label="Top Score" />
-                <Tab label="Players" />
-                <Tab label="Challenges" />
-                <Tab label="Result" />
-                </Tabs>
-            </AppBar>
-            <SwipeableViews
-                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-                index={value}
-                onChangeIndex={handleChangeIndex}
-            >
-                <TabContainer dir={theme.direction}><Topscore /></TabContainer>
-                <TabContainer dir={theme.direction}><Players /></TabContainer>
-                <TabContainer dir={theme.direction}><Challenges /></TabContainer>
-                <TabContainer dir={theme.direction}><Result /></TabContainer>
-            </SwipeableViews>
+  function handleChangeIndex(index) {
+    setValue(index);
+  }
+  return (
+    <div className={classes.root} id="homenav">
+      <AppBar position="static" color="blue" className="appbar">
+        <Tabs className="tabs"
+          value={value}
+          onChange={handleChange}
+          variant="scrollable"
+          scrollButtons="auto"
+        >
+          <Tab label="Top Score" />
+          <Tab label="Players" />
+          <Tab label="Challenges" />
+          <Tab label="Result" />
+          <Tab label="Garage" />
+        </Tabs>
+      </AppBar>
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabContainer dir={theme.direction}><Topscore /></TabContainer>
+        <TabContainer dir={theme.direction}><Players /></TabContainer>
+        <TabContainer dir={theme.direction}><Challenges /></TabContainer>
+        <TabContainer dir={theme.direction}><Result /></TabContainer>
+        <TabContainer dir={theme.direction}><Garage /></TabContainer>
+      </SwipeableViews>
     </div>
-    )
+  )
 }
 
 export default Home
