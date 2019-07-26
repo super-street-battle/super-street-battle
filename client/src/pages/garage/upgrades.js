250e8952c16d69cb197928f6f45c3924151130a9
Annie Pham <anniekpham@yahoo.com>
2019-07-26 11:40:50 -0700

    finish garage css

--- client/src/pages/garage/upgrades.js
+++ client/src/pages/garage/upgrades.js
@@ -8,14 +8,14 @@ const handlekitupgrade = props=> {
         return (
             <>
             <h5>$1000</h5>
-            <button id={props.index} value='1000' onClick={props.handleBodyKit}>Upgrade</button>
+            <button className="upgItemsbtn" id={props.index} value='1000' onClick={props.handleBodyKit}>↑</button>
             </>
         )
     } else if (props.car.bodyKit === 2) {
         return (
             <>
             <h5>$2500</h5>
-            <button id={props.index} value='2500' onClick={props.handleBodyKit}>Upgrade</button>
+            <button className="upgItemsbtn" id={props.index} value='2500' onClick={props.handleBodyKit}>↑</button>
             </>
         )
     }
@@ -32,35 +32,25 @@ export default function Upgrades(props) {
             <Container style={{paddingBottom:'15%'}}>
                 <Row>
                     <Col xs={12} md={6} className='text-left'>
-                        <h1 className='pt-5 pb-5' style={{ textAlign: 'center', color: '#e97718', fontSize: '25px' }}>Upgrades</h1>
+                        <h1 className='upgrages' style={{ textAlign: 'center', color: '#e97718', fontSize: '25px' }}>Upgrades</h1>
                     </Col>
                 </Row>
                
                 <Row className="text-center">
                     <Col xs={4}>
-                        {/* tire */}
                         <h2 className="upgItems" >Tires</h2>
-                        {/* <div style={{borderRadius: '35px', borderStyle: 'solid', borderWidth:'3px', borderColor: 'white'}}> */}
                         <h4 className="upgItems" >{props.car.tire}</h4>
                         <h5 className="upgItems" >${props.info.tireprice[props.index]}</h5>
-                        
-
-                        <button className="upgItems" id={props.index}  value={props.info.tireprice[props.index]} onClick={props.info.handleTire}>Upgrade</button>
-
-                        {/* </div> */}
+                        <button className="upgItemsbtn" id={props.index}  value={props.info.tireprice[props.index]} onClick={props.info.handleTire}>↑</button>
                     </Col>
 
-                {/* engine */}
                     <Col xs={4}>
-
                         <h2 className="upgItems" >Engine</h2>
                         <h4 className="upgItems" >{props.car.engine}</h4>
                         <h5 className="upgItems" >${props.info.engineprice[props.index]}</h5>
-                     <button className="upgItems"  id={props.index}  value={props.info.engineprice[props.index]} onClick={props.info.handleEngine}>Upgrade</button>
-
+                     <button className="upgItemsbtn"  id={props.index}  value={props.info.engineprice[props.index]} onClick={props.info.handleEngine}>↑</button>
                     </Col>
-                    
-                    {/* body kit */}
+
                     <Col xs={4}>
       
                         <h2 className="upgItems"  >Body Kit</h2>
@@ -69,8 +59,8 @@ export default function Upgrades(props) {
 
                     </Col>
                 </Row>
-                {/* button to sell car */}
-                <Row style={{float:'right', marginTop:'7%'}}>
+
+                <Row style={{float:'center', marginTop:'7%'}}>
                     <button  className="sellCar"  id={props.car._id} value={props.index} data-price={props.car.value} onClick={handleselling}>Sell Car</button>
                 </Row>
             </Container>
