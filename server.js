6dc46038df5d541274e7242194df3d92ccfcb879
Annie Pham <anniekpham@yahoo.com>
2019-07-26 12:58:41 -0700

    add Larry's server.js

--- server.js
+++ server.js
@@ -2,22 +2,31 @@ require('dotenv').config()
 const { join } = require('path')
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
-app.use(express.static(join(__dirname, 'client', 'build')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 require('./routes')(app)
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(join(__dirname, 'client', 'build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(join(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 3001
 
-if (port === 3001) {
-    var MONGODB_URI = `mongodb://localhost/streetbattle`
-} else {
-    var MONGODB_URI = process.env.MONGODB_URI
-}
+app.listen(port, () => console.log(`Server started on port ${port}`));
 
-require('mongoose').connect(MONGODB_URI,
-    { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
-    .then(_ => app.listen(port, () => console.log(`server running on port ${port}`)))
-    .catch(e => console.log(e))
+
+// Connect to Mongo
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true
+}) // Adding new mongo url parser
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
