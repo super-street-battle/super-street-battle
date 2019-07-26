2d84abe9b02a21b55a31604208961b8bc81573dc
LTo28 <39893380+LTo28@users.noreply.github.com>
2019-07-25 19:59:49 -0700

    Revert "Deploy4"

--- server.js
+++ server.js
@@ -22,39 +22,3 @@ require('mongoose').connect(MONGODB_URI,
     { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
     .then(_ => app.listen(port, () => console.log(`server running on port ${port}`)))
     .catch(e => console.log(e))
-
-//deployment
-// require('dotenv').config()
-// const { join } = require('path')
-// const express = require('express')
-// const app = express()
-// const mongoose = require('mongoose')
-
-// app.use(express.urlencoded({ extended: true }))
-// app.use(express.json())
-
-
-// // Serve up static assets
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(join(__dirname, 'client', 'build')));
-
-//     app.get('*', (req, res) => {
-//         res.sendFile(join(__dirname, 'client', 'build', 'index.html'));
-//     });
-// }
-
-// require('./routes')(app)
-
-// const port = process.env.PORT || 3001
-
-// app.listen(port, () => console.log(`Server started on port ${port}`));
-
-
-// // Connect to Mongo
-// mongoose.connect(process.env.MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: true
-// }) // Adding new mongo url parser
-//     .then(() => console.log('MongoDB Connected...'))
-//     .catch(err => console.log(err));
